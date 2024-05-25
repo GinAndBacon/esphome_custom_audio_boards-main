deb7df257735b3f9b0a6c9d3fe5249a34454739d
ginandbacon <57306740+GinAndBacon@users.noreply.github.com>
2024-05-24 21:34:56 -0500

    Update board.c
    t2

--- esp32_s3_korvo1/board.c
+++ esp32_s3_korvo1/board.c
@@ -24,63 +24,61 @@
 
 #include "esp_log.h"
 #include "board.h"
-#include "audio_mem.h"
+#include "audio_volume.h"
+#include "periph_button.h"
 #include "periph_sdcard.h"
-#include "periph_adc_button.h"
 #include "tca9554.h"
 
 static const char *TAG = "AUDIO_BOARD";
 
 static audio_board_handle_t board_handle = 0;
 
-audio_board_handle_t audio_board_init(void)
-{
-    ESP_LOGD(TAG, "Initializing the board");
-    if (board_handle) {
-        ESP_LOGW(TAG, "The board has already been initialized!");
-        return board_handle;
-    }
-    board_handle = (audio_board_handle_t) audio_calloc(1, sizeof(struct audio_board_handle));
-    AUDIO_MEM_CHECK(TAG, board_handle, return NULL);
-    board_handle->audio_hal = audio_board_codec_init();
-    board_handle->adc_hal = audio_board_adc_init();
-    ESP_LOGD(TAG, "Initializing the board done");
+audio_board_handle_t audio_board_init(void) {
+  if (board_handle) {
+    ESP_LOGW(TAG, "The board has already been initialized!");
     return board_handle;
+  }
+  board_handle = (audio_board_handle_t) audio_calloc(1, sizeof(struct audio_board_handle));
+  AUDIO_MEM_CHECK(TAG, board_handle, return NULL);
+  board_handle->audio_hal = audio_board_codec_init();
+  board_handle->adc_hal = audio_board_adc_init();
+  return board_handle;
 }
 
-audio_hal_handle_t audio_board_adc_init(void)
-{
-    ESP_LOGD(TAG, "Initializing the adc");
-    audio_hal_codec_config_t audio_codec_cfg = AUDIO_CODEC_ES7210_CONFIG();
-    audio_hal_handle_t adc_hal = audio_hal_init(&audio_codec_cfg, &AUDIO_CODEC_ES7210_DEFAULT_HANDLE);
-    AUDIO_NULL_CHECK(TAG, adc_hal, return NULL);
-    return adc_hal;
+audio_hal_handle_t audio_board_adc_init(void) {
+  audio_hal_codec_config_t audio_codec_cfg = AUDIO_CODEC_DEFAULT_CONFIG();
+  audio_hal_handle_t adc_hal = NULL;
+  adc_hal = audio_hal_init(&audio_codec_cfg, &AUDIO_CODEC_ES7210_DEFAULT_HANDLE);
+  AUDIO_NULL_CHECK(TAG, adc_hal, return NULL);
+  return adc_hal;
 }
 
-audio_hal_handle_t audio_board_codec_init(void)
-{
-    ESP_LOGD(TAG, "Initializing the codec");
-    audio_hal_codec_config_t audio_codec_cfg = AUDIO_CODEC_ES8311_CONFIG();
-    audio_hal_handle_t codec_hal = audio_hal_init(&audio_codec_cfg, &AUDIO_CODEC_ES8311_DEFAULT_HANDLE);
-    AUDIO_NULL_CHECK(TAG, codec_hal, return NULL);
-    return codec_hal;
+audio_hal_handle_t audio_board_codec_init(void) {
+  audio_hal_codec_config_t audio_codec_cfg = AUDIO_CODEC_DEFAULT_CONFIG();
+  audio_hal_handle_t codec_hal = audio_hal_init(&audio_codec_cfg, &AUDIO_CODEC_ES8311_DEFAULT_HANDLE);
+  AUDIO_NULL_CHECK(TAG, codec_hal, return NULL);
+  return codec_hal;
+}
+esp_err_t audio_board_set_volume(audio_board_handle_t board_handle, int volume) {
+  if (!board_handle || !board_handle->audio_hal) {
+    ESP_LOGE(TAG, "Invalid board or audio HAL handle");
+    return ESP_FAIL;
+  }
+  return audio_hal_set_volume(board_handle->audio_hal, volume);
 }
 
-esp_err_t audio_board_key_init(esp_periph_set_handle_t set)
-{
-    periph_adc_button_cfg_t adc_btn_cfg = PERIPH_ADC_BUTTON_DEFAULT_CONFIG();
-    adc_arr_t adc_btn_tag = ADC_DEFAULT_ARR();
-    adc_btn_tag.total_steps = 6;
-    adc_btn_tag.adc_ch = ADC1_CHANNEL_7;
-    int btn_array[7] = {190, 600, 1000, 1375, 1775, 2195, 3000};
-    adc_btn_tag.adc_level_step = btn_array;
-    adc_btn_cfg.arr = &adc_btn_tag;
-    adc_btn_cfg.arr_size = 1;
-    esp_periph_handle_t adc_btn_handle = periph_adc_button_init(&adc_btn_cfg);
-    AUDIO_NULL_CHECK(TAG, adc_btn_handle, return ESP_ERR_ADF_MEMORY_LACK);
-    return esp_periph_start(set, adc_btn_handle);
+esp_err_t audio_board_get_volume(audio_board_handle_t board_handle, int *volume) {
+  if (!board_handle || !board_handle->audio_hal || !volume) {
+    ESP_LOGE(TAG, "Invalid board or audio HAL handle, or null volume pointer");
+    return ESP_FAIL;
+  }
+  return audio_hal_get_volume(board_handle->audio_hal, volume);
 }
 
+static esp_err_t _get_lcd_io_bus(void *bus, esp_lcd_panel_io_spi_config_t *io_config,
+                                 esp_lcd_panel_io_handle_t *out_panel_io) {
+  return esp_lcd_new_panel_io_spi((esp_lcd_spi_bus_handle_t) bus, io_config, out_panel_io);
+}
 esp_err_t audio_board_sdcard_init(esp_periph_set_handle_t set, periph_sdcard_mode_t mode)
 {
     if (mode != SD_MODE_1_LINE) {
